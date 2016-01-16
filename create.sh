6dbf8835ff08f19dbf8f17dc313e81b73a3952d9
Niko Köbler <niko@n-k.de>
2016-01-16 13:52:20 +0100

    setup

--- create.sh
+++ create.sh
@@ -1,5 +1,6 @@
 mkdir -p $HOME/pages
+mkdir -p $HOME/pages/images
 BASEDIR=$(dirname $0)
 cd $BASEDIR
-cp ./images/*.* $HOME/pages/images
+cp ./images/*.* $HOME/pages/images/
 asciidoctor index.adoc -d book -D $HOME/pages
